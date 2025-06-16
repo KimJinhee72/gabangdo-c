cf6b19a9576c47b46d217186a7d5b9850d1adb17
geen <geenhankuk@gmail.com>
2025-06-16 17:09:02 +0900

    언어수정

--- src/main.js
+++ src/main.js
@@ -7,10 +7,6 @@ import router from "./router";
 import { createPinia } from "pinia";
 import i18n from "./i18n"; // ✅ 이걸 그대로 사용
 
-// 사용자가 저장한 언어 가져오기
-const savedLang = localStorage.getItem("lang") || "ko";
-i18n.global.locale = savedLang;
-
 const app = createApp(App);
 const pinia = createPinia();
 
